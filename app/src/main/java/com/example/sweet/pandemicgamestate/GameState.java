dd8edb13c2eef48d9afda1bc89d5b39fc96d3fa7
kelsicruz <cruzk20@up.edu>
2018-10-03 11:25:39 -0700

    second commit -- added instance variables to gamestate class. -K

--- app/src/main/java/com/example/sweet/pandemicgamestate/GameState.java
+++ app/src/main/java/com/example/sweet/pandemicgamestate/GameState.java
@@ -1,7 +1,52 @@
 package com.example.sweet.pandemicgamestate;
 
-//Cards
-//Pawns
+/************************************
+ * GAME STATE VARIABLES
+ *  -Cards: card
+ *  -Pawns: pawn
+ *  -numOfPlayers: int
+ *  -playerTurn: String
+ *  -actionsLeft: int
+ *  -infectionRate: int
+ *  -outbreakNum: int
+ *
+ *
+ ************************************/
 
+
+
+
+/************************************
+ * GameState Constructor
+ * Initializes a 2-player game
+ * where neither players have any
+ * cards or pawns yet and it is
+ * player 1's turn.
+ ************************************/
 public class GameState {
+    GeneralCards p1Cards = null;
+    GeneralCards p2Cards = null;
+    RoleCards p1Role = null;
+    RoleCards p2Role = null;
+    //Pawn p1Pawn = null;
+    //Pawn p2Pawn = null;
+    int numOfPlayers = 2;
+    String playerTurn = "player 1";
+    int infectionRate = 2;
+    int outbreakNum = 0;
+    int actionsLeft = 4;
+
 }
+
+
+
+/*
+//Dummied up toString method because it was
+//giving me issues lol. -K
+
+public String toString(){
+    String gameSS = "";
+
+
+    return gameSS;
+}*/
