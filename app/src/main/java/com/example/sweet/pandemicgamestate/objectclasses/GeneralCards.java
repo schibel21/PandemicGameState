b7752ca2a79c70e0bcc2864647af9a1c8dde6563
polinagannotskaya <gannotsk21@up.edu>
2018-10-22 09:25:02 -0700

    Added toSting method into Game state

--- app/src/main/java/com/example/sweet/pandemicgamestate/objectclasses/GeneralCards.java
+++ app/src/main/java/com/example/sweet/pandemicgamestate/objectclasses/GeneralCards.java
@@ -1,6 +1,40 @@
 package com.example.sweet.pandemicgamestate.objectclasses;
 
+import java.util.HashMap;
+
 public class GeneralCards
 {
+    int cardID;
+    int diseaseColor;
+    City location;
+    boolean infectionCard;
+    //each card is a hash map
+    //it contains cardID, location, disease color
+    //cardID, location, diseaseColor, infectionCard
 
+    public GeneralCards()//int newCardID, City city, int color
+    {
+        //cardID = newCardID;
+        //location = city;
+       // diseaseColor = color;
+    }
+
+    //if infectionCard boolean is true, it's an infection card
+    HashMap<Boolean, GeneralCards> infectionCards= new HashMap<>();
+
+    //if infectionCard boolean is false, its a player card
+    HashMap<Boolean, GeneralCards> playerCards= new HashMap<>();
+
+    /*
+    color 1 = yellow
+    color 2 = blue
+    color 3 = black
+    color 4 = red
+     */
+
+    //GeneralCards card1 = new GeneralCards(getResourceID, cityObject, 1);
+    //playerCards.put(false, card1)
+
+
+    //GeneralCards card1 = new GeneralCards(getResourceID, cityObject, 1);
 }
