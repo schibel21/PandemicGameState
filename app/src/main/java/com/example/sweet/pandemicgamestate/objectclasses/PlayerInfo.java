0d1272f325e66f7a3d5c711dd711adaf725eafe8
polinagannotskaya <gannotsk21@up.edu>
2018-10-25 10:51:30 -0700

    player info is in

--- app/src/main/java/com/example/sweet/pandemicgamestate/objectclasses/PlayerInfo.java
+++ app/src/main/java/com/example/sweet/pandemicgamestate/objectclasses/PlayerInfo.java
@@ -4,20 +4,46 @@ import java.util.ArrayList;
 
 public class PlayerInfo {
 
-    private ArrayList<GeneralCards> playerCards;
-    private String role;
-    private String location;
+        private ArrayList<playerCard> playerHand;
+        private int role;
+        private City currentLocation;
+        private int actionsLeft;
 
-    public PlayerInfo() {
+        public PlayerInfo(int playerRole, int initActions, City startLoc, playerCard card1, playerCard card2) {
+            role = playerRole;
+            actionsLeft = initActions;
+            currentLocation = startLoc;
+            playerHand.add(card1);
+            playerHand.add(card2);
 
-    }
+        }
 
+        public int getRole(){
+            return role;
+        }
 
+        public City getCurrentLocation(){
+            return currentLocation;
+        }
 
-    public String playerLocation(String location){
-        this.location = location;
-        return location;
-    }
+        public void setCurrentLocation(City newLocation){
+            currentLocation = newLocation;
+        }
+
+        public int getActionsLeft(){
+            return actionsLeft;
+        }
+
+        public void actionTaken(){
+            actionsLeft--;
+        }
+
+        public boolean addCardToPlayerHand(playerCard newCard){
+            if(playerHand.size() >= 7){
+                return false;
+            }
+            return true;
+        }
 
 
 }
